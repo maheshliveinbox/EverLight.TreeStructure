a12f2a30d5c96dd38ffe3721c82958ee7ff98ffd
Kapila Mahesh <Mahesh.Gamage@nexa.com.au>
2021-04-19 18:29:06 +1000

    Code Cleanup

---
+++ EverLight.TreStructure.Tests/BusinessLogic/TreeNode_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EverLight.TreeStructure.Core;
+using EverLight.TreeStructure.Core.BusinessLogic;
+
+namespace EverLight.TreStructure.Tests.BusinessLogic
+{
+    public class TreeNode_Tests
+    {
+        private readonly ITreeNode<int> _treeNode;
+        public TreeNode_Tests()
+        {
+            _treeNode = Factory<int>.CreateTreeNode("root");
+        }
+    }
+}
